a9720ce463d13bf1c3952bc585a3d491f6da68c5
abhiraj646 <abhirajsinghchauhan123@gmail.com>
2024-09-14 14:36:09 +0530

    Add files via upload

---
+++ calculater.py
@@ -0,0 +1,20 @@
+print("*********welcome to calculater*****************")
+a=int(input("enter first number"))
+b=int(input("enter second number "))
+print("1=+,2=-,3=*,4=/,5=%")
+c=input("enter operator")
+if c=='1':
+    print(a+b)
+elif c=='2':
+    print(a-b)
+elif c=='3':
+  print(a*b)
+elif c=='4':
+    print(a/b)
+elif c=='5':
+   print(a%b)
+else:
+   print("invalid input")
+print("thank you for using mini project")
+print("calculater")
+print("create by abhiraj singh chauhan")
